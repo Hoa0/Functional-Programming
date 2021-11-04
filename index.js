8690932b8335a240b6a1a5eaa183ecda213fa480
hoa0 <t-hoa.l@hotmail.com>
2021-11-04 14:56:10 +0100

    sad fetch doesnt work

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const dataset = require('./tech-track-dataset.json')
-console.log(dataset)
+import fetch from "node-fetch";
+globalThis.fetch = fetch;
+
+/*const DATASETS = require('./tech-track-dataset.json')
+console.log(DATASETS)*/
+
+fetch("../tech-track-dataset.json")
+.then(res => res.json())
+.then(data => console.log(data))
+
+  
